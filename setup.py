2adb479959a7eea680bfb04cf3a9c1a0fa037a9d
asedler3 <asedler@gatech.edu>
2019-01-03 15:34:14 -0500

    packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='semnet',
+	version='0.1.0',
+	description='A package for working with Semantic Medline data',
+	url='https://github.gatech.edu/pathology-dynamics/semnet',
+	author='Andrew Sedler',
+	author_email='asedler3@gatech.edu',
+	packages=['re', 'hetio', 'xarray', 'numpy', 'collections',
+		'py2neo', 'gzip', 'pickle', 'numpy', 'pandas',
+		'sklearn', 'scipy', 'matplotlib', 'pickle', 'threading',
+		'concurrent', 'tqdm', 'copy', 'math', 'random'])
