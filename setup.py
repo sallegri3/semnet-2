cdf9f07003f2f3675f7a86e80574e259547c9826
asedler3 <asedler@gatech.edu>
2019-01-03 15:48:56 -0500

    fix packaging

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(name='semnet',
 	url='https://github.gatech.edu/pathology-dynamics/semnet',
 	author='Andrew Sedler',
 	author_email='asedler3@gatech.edu',
-	packages=['re', 'hetio', 'xarray', 'numpy', 'collections',
+	packages=['semnet']
+	install_requires=['re', 'hetio', 'xarray', 'numpy', 'collections',
 		'py2neo', 'gzip', 'pickle', 'numpy', 'pandas',
 		'sklearn', 'scipy', 'matplotlib', 'pickle', 'threading',
 		'concurrent', 'tqdm', 'copy', 'math', 'random'])
