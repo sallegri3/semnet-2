af2285bf750323772e513ba35be3a99c755276e2
asedler3 <asedler@gatech.edu>
2019-02-04 08:47:50 -0500

    changing theme to default

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -78,7 +78,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -87,7 +88,7 @@ html_theme = 'alabaster'
 # html_theme_options = {}
 
 html_theme_options = {
-    'logo': 'pathology_dynamics.png'
+    # 'logo': 'pathology_dynamics.png'
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
