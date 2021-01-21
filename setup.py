7699c2ada285062900d34e0bf7167b6e56b17707
Kartchner_David_S_dkartchner3 <dkartchner3@MITCH-LIN.AD.GATECH.EDU>
2021-01-21 15:28:15 -0500

    update dependencies

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(name='semnet',
 	author='Andrew Sedler',
 	author_email='asedler3@gatech.edu',
 	packages=['semnet'],
-	install_requires=['hetio==0.2.8', 'xarray==0.10.7', 'numpy==1.15.0','py2neo==3.1.2', 'pandas==0.23.0', 
-	'sklearn==0.19.1', 'scipy==1.1.0', 'matplotlib==2.2.2', 'tqdm==4.23.4', 'seaborn==0.8.1'],
+	install_requires=['hetio==0.2.8', 'xarray==0.16.2', 'numpy==1.19.5','py2neo==3.1.2', 'pandas==1.1.5', 
+	'scikit-learn>=0.19.1', 'scipy==1.5.4', 'matplotlib==3.3.3', 'tqdm>=4.23.4', 'seaborn==0.8.1'],
 	include_package_data=True,
 	package_data={
 		'semnet': ['data/*']
