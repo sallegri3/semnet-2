673b09c3dd06db13c409294a9fe29323a90b55e4
Anna Kirkpatrick <akirkpatrick3@gatech.edu>
2021-05-01 18:25:24 -0400

    draft of restricted random walk on mp

--- semnet/randomized_hetesim.py
+++ semnet/randomized_hetesim.py
@@ -34,6 +34,7 @@ def random_walk_on_metapath(graph, start_node, metapath, walk_forward=True):
             dead_end: True if the path hit a dead end; false if it made it to the end of the metapath
             node: the node arrived at when the end of the metapath is reached, or the dead end node
     """
+
     path_len = (len(metapath)-1)/2
     i=1
     cur_node = start_node
@@ -91,6 +92,33 @@ def random_walk_on_metapath(graph, start_node, metapath, walk_forward=True):
             node: the node arrived at when the end of the metapath is reached, or the dead end node
     """
 
+    path_len = (len(metapath)-1)/2
+    i=1
+    cur_node = start_node
+    if walk_forward:
+        neighbors = list( graph.outgoing_edges[curr_node][metapath[2*i -1]][metapath[2*i]] - bad_nodes[i] ) # set of neighbors of curr_node under the next relation in the metapath, except those in bad_nodes
+    else:
+        neighbors = list( graph.incoming_edges[curr_node][metapath[2*path_len + 1 - 2*i]][metapath[2*path_len - 2*i]] - bad_nodes[i] )
+    
+    while i <= path_len and len(neighbors) > 0:
+        if walk_forward:
+            edge_weights = [graph.incoming_edge_weights[cur_node][metapath[2*i - 1]][y] for y in neighbors]
+        else:
+            edge_weights = [graph.outgoing_edge_weights[cur_node][metapath[2*path_len + 1 - 2*i]][y] for y in neighbors]
+    
+        cur_node = random.choices(neighbors, weights=edge_weights) 
+        i+=1
+
+        if i == path_len +1:
+            return (i-1, cur_node)
+
+        if walk_forward: 
+            neighbors = list( graph.outgoing_edges[curr_node][metapath[2*i -1]][metapath[2*i]] - bad_nodes[i-1]) 
+        else:
+            neighbors = list( graph.incoming_edges[curr_node][metapath[2*path_len + 1 - 2*i]][metapath[2*path_len - 2*i]] - bad_nodes[i])
+        
+    return (i-1, cur_node)
+    
 def randomized_hetesim(graph, start_rodes, end_nodes, metapaths, k_left, k_right, epsilon, r, g):
     """
     Randomized implementation of HeteSim,
